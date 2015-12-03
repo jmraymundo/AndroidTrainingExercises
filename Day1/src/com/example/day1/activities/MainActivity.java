51a2d95d036a9d06132b8d8dc530ea89b9d7fb5d
Jan Michael Raymundo <j.raymundo@klab.com>
2015-12-03 14:29:47 +0800

    Added MainActivity

---
+++ Day1/src/com/example/day1/activities/MainActivity.java
@@ -0,0 +1,17 @@
+
+package com.example.day1.activities;
+
+import com.example.day1.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity
+{
+    @Override
+    protected void onCreate( Bundle savedInstanceState )
+    {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.main );
+    }
+}

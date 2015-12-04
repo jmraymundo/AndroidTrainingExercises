f04e216425d7f1f9cbb125bec295d5afc1c29c0e
Jan Michael Raymundo <j.raymundo@klab.com>
2015-12-04 10:58:26 +0800

    Added new activity and layout. Layout contains 3 buttons for calling a Spinner, DatePicker, and TimePicker. Also contains 3 TextFields, with 3 different properties (default, for a person's name, and for passwords). Also contains a CheckBox and a RadioButton.

---
+++ Day2-InputControls/src/com/example/day2_inputcontrols/activities/MainActivity.java
@@ -0,0 +1,18 @@
+
+package com.example.day2_inputcontrols.activities;
+
+import com.example.day2_inputcontrols.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity
+{
+
+    @Override
+    protected void onCreate( Bundle savedInstanceState )
+    {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_main );
+    }
+}

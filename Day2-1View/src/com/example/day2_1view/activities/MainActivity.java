26b2c3dd2b84227facf0a103ca086bf4b8fa22b4
Jan Michael Raymundo <j.raymundo@klab.com>
2015-12-04 15:50:49 +0800

    Added Toast and Logs.

--- Day2-1View/src/com/example/day2_1view/activities/MainActivity.java
+++ Day2-1View/src/com/example/day2_1view/activities/MainActivity.java
@@ -5,12 +5,14 @@ import com.example.day2_1view.R;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends Activity
+public class MainActivity extends Activity implements OnClickListener
 {
 
     TextView tvOut;
@@ -18,6 +20,8 @@ public class MainActivity extends Activity
     Button btnOk;
 
     Button btnCancel;
+
+    private static final String TAG = "myLogs";
 
     /** Called when the activity is first created. */
     @Override
@@ -27,29 +31,35 @@ public class MainActivity extends Activity
         setContentView( R.layout.activity_main );
 
         // find View-elements
+        Log.d( TAG, "find View-elements" );
         tvOut = ( TextView ) findViewById( R.id.tvOut );
         btnOk = ( Button ) findViewById( R.id.btnOk );
-        btnOk.setOnClickListener( oclBtnOk );
         btnCancel = ( Button ) findViewById( R.id.btnCancel );
-        btnCancel.setOnClickListener( oclBtnCancel );
+
+        // assign listener to buttons
+        Log.d( TAG, "assign lstener to buttons" );
+        btnOk.setOnClickListener( this );
+        btnCancel.setOnClickListener( this );
     }
 
-    OnClickListener oclBtnOk = new OnClickListener()
+    public void onClick( View v )
     {
-        @Override
-        public void onClick( View v )
+        // define the button that invoked the listener by id
+        Log.d( TAG, "define the button that invoked the listener by id" );
+        switch( v.getId() )
         {
-            tvOut.setText( "Button OK clicked" );
+            case R.id.btnOk:
+                // ОК button
+                Log.d( TAG, "ОК button" );
+                tvOut.setText( "ОК button was clicked" );
+                Toast.makeText( this, "ОК button was clicked", Toast.LENGTH_LONG ).show();
+                break;
+            case R.id.btnCancel:
+                // Cancel button
+                Log.d( TAG, "Cancel button" );
+                tvOut.setText( "Cancel button was clicked" );
+                Toast.makeText( this, "Cancel button was clicked", Toast.LENGTH_LONG ).show();
+                break;
         }
-    };
-
-    OnClickListener oclBtnCancel = new OnClickListener()
-    {
-        @Override
-        public void onClick( View v )
-        {
-            // Change text of TextView (tvOut)
-            tvOut.setText( "Button Cancel clicked" );
-        }
-    };
+    }
 }

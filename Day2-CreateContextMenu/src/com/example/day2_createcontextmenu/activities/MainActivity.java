eb66c8299c05f7c74b3abc4519c2a17628b84e7a
Jan Michael Raymundo <j.raymundo@klab.com>
2015-12-04 16:46:57 +0800

    Added listeners to the context menus.

--- Day2-CreateContextMenu/src/com/example/day2_createcontextmenu/activities/MainActivity.java
+++ Day2-CreateContextMenu/src/com/example/day2_createcontextmenu/activities/MainActivity.java
@@ -4,9 +4,11 @@ package com.example.day2_createcontextmenu.activities;
 import com.example.day2_createcontextmenu.R;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.ContextMenu;
 import android.view.ContextMenu.ContextMenuInfo;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
@@ -59,5 +61,41 @@ public class MainActivity extends Activity
                 menu.add( 0, MENU_SIZE_30, 0, "30" );
                 break;
         }
+    }
+
+    @Override
+    public boolean onContextItemSelected( MenuItem item )
+    {
+        // TODO Auto-generated method stub
+        switch( item.getItemId() )
+        {
+            // menu items for tvColor
+            case MENU_COLOR_RED:
+                tvColor.setTextColor( Color.RED );
+                tvColor.setText( "Text color = red" );
+                break;
+            case MENU_COLOR_GREEN:
+                tvColor.setTextColor( Color.GREEN );
+                tvColor.setText( "Text color = green" );
+                break;
+            case MENU_COLOR_BLUE:
+                tvColor.setTextColor( Color.BLUE );
+                tvColor.setText( "Text color = blue" );
+                break;
+            // menu items for tvSize
+            case MENU_SIZE_22:
+                tvSize.setTextSize( 22 );
+                tvSize.setText( "Text size = 22" );
+                break;
+            case MENU_SIZE_26:
+                tvSize.setTextSize( 26 );
+                tvSize.setText( "Text size = 26" );
+                break;
+            case MENU_SIZE_30:
+                tvSize.setTextSize( 30 );
+                tvSize.setText( "Text size = 30" );
+                break;
+        }
+        return super.onContextItemSelected( item );
     }
 }

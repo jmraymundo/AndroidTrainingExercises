9f7eaed1b2f22076447c99c094ecabd2222597e2
Jan Michael Raymundo <j.raymundo@klab.com>
2015-12-04 17:05:26 +0800

    Created new Activity. Added functionality to be able to start a new activity on top of the first.

--- Day2-ActivityPart2/src/com/example/day2_activitypart2/activities/MainActivity.java
+++ Day2-ActivityPart2/src/com/example/day2_activitypart2/activities/MainActivity.java
@@ -1,8 +1,42 @@
 
 package com.example.day2_activitypart2.activities;
 
-import android.app.Activity;
+import com.example.day2_activitypart2.R;
 
-public class MainActivity extends Activity
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainActivity extends Activity implements OnClickListener
 {
+
+    Button btnActTwo;
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate( Bundle savedInstanceState )
+    {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_main );
+
+        btnActTwo = ( Button ) findViewById( R.id.btnActTwo );
+        btnActTwo.setOnClickListener( this );
+    }
+
+    @Override
+    public void onClick( View v )
+    {
+        switch( v.getId() )
+        {
+            case R.id.btnActTwo:
+                Intent intent = new Intent( this, ActivityTwo.class );
+                startActivity( intent );
+                break;
+            default:
+                break;
+        }
+    }
 }

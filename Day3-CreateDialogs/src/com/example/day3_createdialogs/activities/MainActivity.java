6b31e03e6a805674614aafbe32395b5e940e5f03
Jan Michael Raymundo <j.raymundo@klab.com>
2015-12-08 12:12:24 +0800

    Added Dialog with 3 action buttons.

--- Day3-CreateDialogs/src/com/example/day3_createdialogs/activities/MainActivity.java
+++ Day3-CreateDialogs/src/com/example/day3_createdialogs/activities/MainActivity.java
@@ -4,39 +4,65 @@ package com.example.day3_createdialogs.activities;
 import com.example.day3_createdialogs.R;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends Activity
 {
+    Button mButton1;
+
+    Button mButton2;
+
+    Button mButton3;
 
     @Override
     protected void onCreate( Bundle savedInstanceState )
     {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_main );
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu( Menu menu )
-    {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate( R.menu.main, menu );
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected( MenuItem item )
-    {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if( id == R.id.action_settings )
+        mButton1 = ( Button ) findViewById( R.id.button1 );
+        mButton1.setOnClickListener( new OnClickListener()
         {
-            return true;
-        }
-        return super.onOptionsItemSelected( item );
+            @Override
+            public void onClick( View v )
+            {
+
+                AlertDialog.Builder dialogBuilder = new AlertDialog.Builder( MainActivity.this );
+                dialogBuilder.setTitle( "Are you hungry?" );
+                dialogBuilder.setPositiveButton( "Yes", new DialogInterface.OnClickListener()
+                {
+                    @Override
+                    public void onClick( DialogInterface dialog, int which )
+                    {
+                        Toast.makeText( MainActivity.this, "You are hungry.", Toast.LENGTH_SHORT ).show();
+                    }
+                } );
+                dialogBuilder.setNeutralButton( "Maybe", new DialogInterface.OnClickListener()
+                {
+                    @Override
+                    public void onClick( DialogInterface dialog, int which )
+                    {
+                        Toast.makeText( MainActivity.this, "You may be hungry.", Toast.LENGTH_SHORT ).show();
+                    }
+                } );
+                dialogBuilder.setNegativeButton( "No", new DialogInterface.OnClickListener()
+                {
+                    @Override
+                    public void onClick( DialogInterface dialog, int which )
+                    {
+                        Toast.makeText( MainActivity.this, "You are not hungry.", Toast.LENGTH_SHORT ).show();
+                    }
+                } );
+                AlertDialog dialog = dialogBuilder.create();
+                dialog.show();
+            }
+        } );
+        mButton2 = ( Button ) findViewById( R.id.button2 );
+        mButton3 = ( Button ) findViewById( R.id.button3 );
     }
 }

567d481e05017dcebcd25b8831d2a9bf49729584
Jan Michael Raymundo <j.raymundo@klab.com>
2015-12-09 14:56:45 +0800

    Added json file to parse. Since json file is ~20MB, utilized JsonReader to parse.

--- Day4-JSONParsing/src/com/example/day4_jsonparsing/activities/MainActivity.java
+++ Day4-JSONParsing/src/com/example/day4_jsonparsing/activities/MainActivity.java
@@ -1,12 +1,17 @@
 
 package com.example.day4_jsonparsing.activities;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
 import com.example.day4_jsonparsing.R;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.JsonReader;
+import android.util.Log;
 
 public class MainActivity extends Activity
 {
@@ -15,5 +20,77 @@ public class MainActivity extends Activity
     {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_main );
+        getJSONString();
+
+    }
+
+    private String getJSONString()
+    {
+        String output = null;
+        try
+        {
+            InputStream is = getAssets().open( "units_info.json" );
+            InputStreamReader isReader = new InputStreamReader( is );
+            JsonReader reader = new JsonReader( isReader );
+            List< Unit > topTen = new ArrayList< Unit >();
+            reader.beginObject();
+            while( reader.hasNext() )
+            {
+                String unitId = reader.nextName();
+                String unitName = null;
+                String unitLordDmgRange = null;
+                reader.beginObject();
+                while( reader.hasNext() )
+                {
+                    String propertyName = reader.nextName();
+                    if( propertyName.equals( "name" ) )
+                    {
+                        unitName = reader.nextString();
+                    }
+                    else if( propertyName.equals( "lord damage range" ) )
+                    {
+                        unitLordDmgRange = reader.nextString();
+                    }
+                    else
+                    {
+                        reader.skipValue();
+                    }
+                }
+                reader.endObject();
+                topTen.add( new Unit( unitName, unitId, unitLordDmgRange ) );
+            }
+            reader.close();
+            for( Unit unit : topTen )
+            {
+                Log.d( "JSONParsing", ( topTen.indexOf( unit ) + 1 ) + " " + unit.toString() );
+            }
+        }
+        catch( Exception e )
+        {
+            Log.e( "JSONParsing", "Exception! " + e );
+        }
+        return output;
+    }
+
+    private class Unit
+    {
+        String mId;
+
+        String mName;
+
+        String mLordDmg;
+
+        public Unit( String id, String name, String lordDmg )
+        {
+            mId = id;
+            mName = name;
+            mLordDmg = lordDmg;
+        }
+
+        @Override
+        public String toString()
+        {
+            return "Unit name: " + mName + " Unit ID: " + mId + " Ave. damage: " + mLordDmg;
+        }
     }
 }

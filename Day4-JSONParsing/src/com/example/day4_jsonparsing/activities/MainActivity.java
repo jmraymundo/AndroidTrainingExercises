385185b33209c80511b9cc98e4c97fd5a9b23d03
Jan Michael Raymundo <j.raymundo@klab.com>
2015-12-09 13:42:04 +0800

    Updated new project for JSON parsing exercises.

---
+++ Day4-JSONParsing/src/com/example/day4_jsonparsing/activities/MainActivity.java
@@ -0,0 +1,19 @@
+
+package com.example.day4_jsonparsing.activities;
+
+import com.example.day4_jsonparsing.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity
+{
+    @Override
+    protected void onCreate( Bundle savedInstanceState )
+    {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_main );
+    }
+}
